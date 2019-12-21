77bb6e39b9a9a083acf8b068081c41e4060133cd
Leo Newball, Jr <contact@leonewball.com>
2019-12-21 11:31:33 -0500

    Initial Commit

---
+++ war.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * PHP Version of the card game War
+ * Creator: Leo Newball
+ * Description: This is a version of the card game War, where a deck is shuffled and 
+ * split between two players. Then, the top card of each deck battles against each others
+ * where the higher number beats out the lower number.
+ * 
+ */
+
+
+
+
+?>
